08a6bcd27676753a1aaa264251272f5a95e951a1
manhnguyen0502 <nguyentrongmanh050298@gmail.com>
2019-09-10 23:05:59 +0700

    xxx

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const mongooes = require('mongoose');
 const schema = require("./schema/schema");
-const express = require("express");
-const { ApolloServer } = require('apollo-server-express');
+const { ApolloServer } = require('apollo-server');
 mongooes.connect("mongodb+srv://admin123:admin123@cluster0-hmgvh.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true });
 mongooes.connection.once('open', () => {
 	console.log('DB CONNECTD');
@@ -12,19 +11,10 @@ const server = new ApolloServer({
 	cors: {
 		origin: '*',            // <- allow request from all domains
 		credentials: true
-	},
-	playground: true
-});
-
-const app = express();
-
-app.get("*", (req, res) => {
-	res.send("1234");
+	}
 });
 
 var port = process.env.PORT || 5000;
-
-server.applyMiddleware({ app });
 
 server.listen({ port: port }).then(({ url }) => {
 	console.log(`🚀 Server ready at ${url}`)
